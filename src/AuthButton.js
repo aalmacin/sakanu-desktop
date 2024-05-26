ecbb1153384722a889707021e10e4c57d2460487
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2024-05-25 18:07:30 -0600

    Consent is moved on login

--- src/AuthButton.js
+++ src/AuthButton.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useCallback} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from '@mui/material';
 
 const AuthButton = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
+    const login = useCallback(() => {
+        loginWithRedirect({
+            authorizationParams: {
+                audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+                scope: "openid email profile",
+            },
+        })
+    }, [loginWithRedirect]);
+
     return isAuthenticated ? (
         <Button variant="contained" color="secondary" onClick={() => logout({ returnTo: window.location.origin })}>
             Log Out
         </Button>
     ) : (
-        <Button variant="contained" color="primary" onClick={loginWithRedirect}>
+        <Button variant="contained" color="primary" onClick={login}>
             Log In
         </Button>
     );
--- src/auth0/useAuth0Consent.js
+++ src/auth0/useAuth0Consent.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react';
 import {useAuth0} from "@auth0/auth0-react";
 
 export const useAuth0Consent = () => {
-    const {getAccessTokenSilently, getAccessTokenWithPopup} = useAuth0();
+    const {getAccessTokenSilently} = useAuth0();
     const [token, setToken] = useState(null);
 
     useEffect(() => {
@@ -15,20 +15,9 @@ export const useAuth0Consent = () => {
         }).then(token => {
             setToken(token);
         }).catch(error => {
-            if (error.error === 'consent_required') {
-                getAccessTokenWithPopup({
-                    authorizationParams: {
-                        audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-                        scope: "openid email profile"
-                    }
-                }).then(token => {
-                    setToken(token);
-                }).catch(error => {
-                    console.error('Failed to get token from popup!', error);
-                });
-            }
+            console.error('There was an error!', error);
         });
-    }, [getAccessTokenSilently, getAccessTokenWithPopup]);
+    }, [getAccessTokenSilently]);
 
     return {token};
 }
